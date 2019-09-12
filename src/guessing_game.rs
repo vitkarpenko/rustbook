6d991aeafb41556cfcf1e0823000e1d2f89e1b0f
Vitaly Karpenko <vitkarpenko@gmail.com>
2019-09-12 16:51:00 +0300

    guessing game

--- src/guessing_game.rs
+++ src/guessing_game.rs
@@ -1,14 +1,33 @@
+use std::cmp::Ordering;
 use std::io;
 
+use rand::Rng;
+
 pub fn guessing_game() {
+    let secret_number = rand::thread_rng().gen_range(1, 11);
     println!("Hey! Guess the number or die :|");
-    println!("Please input your guess poor human:");
 
-    let mut guess = String::new();
+    loop {
+        println!("Please input your guess poor human:");
 
-    let stdin = io::stdin();
-    stdin.read_line(&mut guess)
-        .expect("I can't read this...");
+        let mut guess = String::new();
 
-    println!("You guessed: {}", guess);
+        let stdin = io::stdin();
+        stdin.read_line(&mut guess)
+            .expect("I can't read this...");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue
+        };
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Nope. Too small, that's what she said."),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+            Ordering::Greater => println!("Bruh that's too much..."),
+        };
+    }
 }
